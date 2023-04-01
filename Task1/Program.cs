5a5ed05409ad8757b4690bff1e7cb5e409a7d7b4
Алексеев Игорь <dimka_Alexs@mail.ru>
2023-04-01 16:20:46 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число :  ");
+var input1 = Console.ReadLine();
+var number1 = int.Parse(input1);
+Console.WriteLine("Введите второе число:  ");
+var input2 = Console.ReadLine();
+var number2 = int.Parse(input2);
+ 
+if (number1 > number2)
+{
+    Console.WriteLine(number1);
+}
+else;
+if (number2 > number1)
+{
+    Console.WriteLine(number2);
+}
